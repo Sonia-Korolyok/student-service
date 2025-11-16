41e8198606bd4e6c240992fc28ce85f7543bc0fe
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-11-16 12:02:08 +0200

    classwork + homework

--- src/repository/studentRepository.js
+++ src/repository/studentRepository.js
@@ -36,8 +36,8 @@ export const findByName = async (name) => {
     return await collection.find({name: name}).toArray();
 }
 
-export const countByNames = async () => {
-    return await collection.countDocuments({});
+export const countByNames = async (names) => {
+    return await collection.countDocuments({name: { $in: names }});
 }
 
 export const findByMinScore = async (exam, minScore) => {
